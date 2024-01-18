1fb3a0769264a5bec3051e28fdd36375aeaad8ec
mzettersten <mzettersten@users.noreply.github.com>
2024-01-18 09:47:00 -0500

    add mb5 experiment tester

---
+++ experiment/js/example_trial_list_short.js
@@ -0,0 +1,68 @@
+var trial_list = [
+ {
+   "trial_number": 1,
+   "familiar_stimulus": "fractal_complex_pair1_a.png",
+   "novel_stimulus": "fractal_complex_pair1_b.png",
+   "complexity_condition": "complex",
+   "familiarization_time": 5000,
+   "familiar_location_1": "left",
+   "familiar_location_2": "right",
+   "test_time_1": 5000,
+   "test_time_2": 5000
+ },
+ {
+   "trial_number": 2,
+   "familiar_stimulus": "fractal_simple_pair2_b.png",
+   "novel_stimulus": "fractal_simple_pair2_a.png",
+   "complexity_condition": "simple",
+   "familiarization_time": 15000,
+   "familiar_location_1": "right",
+   "familiar_location_2": "left",
+   "test_time_1": 5000,
+   "test_time_2": 5000
+ },
+ {
+   "trial_number": 3,
+   "familiar_stimulus": "fractal_simple_pair3_b.png",
+   "novel_stimulus": "fractal_simple_pair3_a.png",
+   "complexity_condition": "simple",
+   "familiarization_time": 5000,
+   "familiar_location_1": "left",
+   "familiar_location_2": "right",
+   "test_time_1": 5000,
+   "test_time_2": 5000
+ },
+ {
+   "trial_number": 4,
+   "familiar_stimulus": "fractal_complex_pair4_a.png",
+   "novel_stimulus": "fractal_complex_pair4_b.png",
+   "complexity_condition": "complex",
+   "familiarization_time": 15000,
+   "familiar_location_1": "left",
+   "familiar_location_2": "right",
+   "test_time_1": 5000,
+   "test_time_2": 5000
+ },
+ {
+   "trial_number": 5,
+   "familiar_stimulus": "fractal_complex_pair5_b.png",
+   "novel_stimulus": "fractal_complex_pair5_a.png",
+   "complexity_condition": "complex",
+   "familiarization_time": 10000,
+   "familiar_location_1": "right",
+   "familiar_location_2": "left",
+   "test_time_1": 5000,
+   "test_time_2": 5000
+ },
+ {
+   "trial_number": 6,
+   "familiar_stimulus": "fractal_simple_pair6_a.png",
+   "novel_stimulus": "fractal_simple_pair6_b.png",
+   "complexity_condition": "simple",
+   "familiarization_time": 10000,
+   "familiar_location_1": "right",
+   "familiar_location_2": "left",
+   "test_time_1": 5000,
+   "test_time_2": 5000
+ }
+];
